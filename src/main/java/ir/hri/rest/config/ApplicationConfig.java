6e79d1a6fb52fbdf816dc0e698cd72ad3f26e3ad
HR <ireh.hr@gmail.com>
2016-11-20 12:14:50 +0330

    فعال کردن Wadl

--- src/main/java/ir/hri/rest/config/ApplicationConfig.java
+++ src/main/java/ir/hri/rest/config/ApplicationConfig.java
@@ -17,6 +17,7 @@ public class ApplicationConfig extends Application {
 
         resources.add(org.glassfish.jersey.moxy.json.MoxyJsonFeature.class);
         resources.add(JsonMoxyConfigurationContextResolver.class);
+        resources.add(CustomWadlGeneratorConfig.class);
 
         resources.add(BaseInfo.class);
 
@@ -33,15 +34,7 @@ public class ApplicationConfig extends Application {
     public Map<String, Object> getProperties() {
         Map<String, Object> properties = new HashMap<>();
 
-        //in Jersey WADL generation is enabled by default, but we don't
-        //want to expose too much information about our apis.
-        //therefore we want to disable wadl (http://localhost:8080/service/application.wadl should return http 404)
-        //see https://jersey.java.net/nonav/documentation/latest/user-guide.html#d0e9020 for details
-        properties.put("jersey.config.server.wadl.disableWadl", true);
-
-        //we could also use something like this instead of adding each of our resources
-        //explicitely in getClasses():
-        //properties.put("jersey.config.server.provider.packages", "com.nabisoft.tutorials.mavenstruts.service");
+        properties.put("jersey.config.server.wadl.disableWadl", false);
 
         return properties;
     }
